e5de9ecbedb186478e19aeceb25dbc904d228bfd
jrpillai <jayanrpillai@gmail.com>
2024-11-15 11:49:42 -0600

    error logging

--- controllers/demoController.js
+++ controllers/demoController.js
@@ -6,8 +6,17 @@ demoController.getGreeting = (req, res) => {
   return res.status(200).json({ message: 'Hello from the new controller!' });
 };
 
-demoController.createMessage = (req, res) => {
+demoController.createMessage = (req, res, next) => {
   const { message } = req.body;
+
+  if (message === undefined) {
+    return next({
+      log: 'demoController.createMessage: Missing message in request body',
+      status: 400,
+      message: { err: 'Message is required' },
+    });
+  }
+
   return res.status(201).json({ message: `Created message: ${message}` });
 };
 
